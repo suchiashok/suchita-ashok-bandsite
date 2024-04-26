ef52131fb3f1d3d84b98af6d0cf85a4cc4fab13c
suchi ashok <suchiashok.97@gmail.com>
2024-04-26 00:41:02 -0400

    Updating API for comments form

---
+++ scripts/band-site-api.js
@@ -0,0 +1,27 @@
+
+export default class BandSiteApi {
+    constructor(apiKey) {
+        // this.apiKey = apiKey;
+        this.commentUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com/comments?api_key=37a62273-2e78-413f-befb-0357b75e2328';
+    }
+
+//method to post the comments
+    async postComment(commentObj) {
+        try {
+            const response = await axios.post(this.commentUrl, commentObj)
+            return response.data;
+        }
+        catch(error) {
+            console.log('Error',error);
+        }
+    }
+// method to get the comments
+    async getComment() {
+        try {
+            const response = await axios.get(this.commentUrl)
+        return response.data;
+        } catch (error) {
+            console.log('Error', error);
+        }
+    }
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,29 +1,43 @@
+import BandSiteApi from "./band-site-api.js";
+
 const form = document.querySelector(".comment__form");
+const BandApi = new BandSiteApi("37a62273-2e78-413f-befb-0357b75e2328");
 // Default three comments
-const commentArray = [
-    {name: "Victor Pinto", 
-    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-    date: "11/02/2023"},
-    {name: "Christina Cabrera",
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    date: "10/28/2023"},
-    {name: "Isaac Tadesse",
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    date: "10/20/2023"}
-];
+// const commentArray = [
+//     {name: "Victor Pinto", 
+//     comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//     date: "11/02/2023"},
+//     {name: "Christina Cabrera",
+//     comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//     date: "10/28/2023"},
+//     {name: "Isaac Tadesse",
+//     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//     date: "10/20/2023"}
+// ];
 
 const commentPost = document.createElement("div");
 commentPost.classList.add("comment__post");
 document.querySelector(".comment").appendChild(commentPost);
 
 
-function renderNewComments() {
-    commentPost.innerHTML = "";
-    commentArray.forEach(commentObj => {
-        renderComments(commentObj);
-    });
-}
+// function renderNewComments() {
+//     commentPost.innerHTML = "";
+//     commentArray.forEach(commentObj => {
+//         renderComments(commentObj);
+//     });
+// }
 
+async function renderNewComments() {
+    try {
+        const comments = await BandApi.getComment();
+        commentPost.innerHTML = "";
+        comments.forEach(commentObj => {
+            renderComments(commentObj);
+    });
+} catch (error) {
+    console.log('Error', error);
+}
+}
 
 // function to renderComments
 function renderComments(commentObj) {
@@ -63,7 +77,7 @@ commentNameDateDiv.appendChild(commentDate);
 
 renderNewComments();
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
     e.preventDefault()
 
 //Getting the inputs from the form
@@ -78,10 +92,19 @@ const newComment = {
     date: dateLabel
 };
 
-commentArray.unshift(newComment);
-renderNewComments();
-document.getElementById("nameInput").value = "";
-document.getElementById("commentInput").value = "";
+// commentArray.unshift(newComment);
+// renderNewComments();
+// document.getElementById("nameInput").value = "";
+// document.getElementById("commentInput").value = "";
 
+// });
+// console.log(commentArray);
+try {
+    await BandApi.postComment(newComment);
+    await renderNewComments();
+    document.getElementById("nameInput").value = "";
+    document.getElementById("commentInput").value = "";
+} catch (error) {
+    console.log("Error", error);
+}
 });
-console.log(commentArray);
